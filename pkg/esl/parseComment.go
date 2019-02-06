23d15532f738a239d520a99c895164253668ab1e
robert.reppel <robert.reppel@twotonic.net>
2019-02-06 12:44:47 -0800

    Honour (or at least ignore) /// workspace/eventstorming names.

--- pkg/esl/parseComment.go
+++ pkg/esl/parseComment.go
@@ -9,7 +9,10 @@ func parseComment(eslInput string, lineItems []Item) []Item {
 	re, _ := regexp.Compile("^ *// *(.*)")
 	comment := re.FindAllStringSubmatch(eslInput, -1)
 	if len(comment) > 0 {
-		lineItems = append(lineItems, Comment{Text: strings.Trim(comment[0][1], " ")})
+		commentText := strings.Trim(comment[0][1], " ")
+		commentText = strings.TrimLeft(commentText, "/")
+		commentText = strings.TrimLeft(commentText, " ")
+		lineItems = append(lineItems, Comment{Text: commentText})
 	}
 	return lineItems
 }
